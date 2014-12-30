45b19db4a142e0c754fc3b10f69f5b2f8b961ed0
jjperry69 <jjperry69@gmail.com>
2014-12-30 15:48:11 -0500

    Updates to HP-UX Provider for Trusted Systems. After changing password hash, it resets the password aging counter to 0. This resolves issue around passwords showing expired after changed.

--- lib/puppet/provider/user/hpux.rb
+++ lib/puppet/provider/user/hpux.rb
@@ -6,10 +6,11 @@ Puppet::Type.type(:user).provide :hpuxuseradd, :parent => :useradd do
   defaultfor :operatingsystem => "hp-ux"
   confine :operatingsystem => "hp-ux"
 
-  commands :modify => "/usr/sam/lbin/usermod.sam", :delete => "/usr/sam/lbin/userdel.sam", :add => "/usr/sam/lbin/useradd.sam"
+  commands :modify => "/usr/sam/lbin/usermod.sam", :delete => "/usr/sam/lbin/userdel.sam", :add => "/usr/sam/lbin/useradd.sam", :password => '/usr/lbin/modprpw', :expiry => '/usr/lbin/modprpw'
   options :comment, :method => :gecos
   options :groups, :flag => "-G"
   options :home, :flag => "-d", :method => :dir
+  options :expiry, :method => :expire
 
   verify :gid, "GID must be an integer" do |value|
     value.is_a? Integer
@@ -19,48 +20,102 @@ Puppet::Type.type(:user).provide :hpuxuseradd, :parent => :useradd do
     value !~ /\s/
   end
 
-  has_features :manages_homedir, :allows_duplicates, :manages_passwords
+  has_features :manages_homedir, :allows_duplicates, :manages_passwords, :manages_password_age, :manages_expiry
 
   def deletecmd
     super.insert(1,"-F")
   end
 
   def modifycmd(param,value)
-    super.insert(1,"-F")
+     cmd = super(param, value)
+     cmd << "-F"
+     if self.trusted == "Trusted"
+        cmd << ";"
+        cmd << "/usr/lbin/modprpw"
+        cmd << "-v"
+        cmd << "-l"
+        cmd << "#{resource.name}"
+     end
+#     cmd.each do |i|
+#         pp "****#{i}*****"
+#     end
+     cmd
   end
 
   def password
-      # Password management routine for trusted and non-trusted systems
-      #temp=""
-      while ent = Etc.getpwent() do
-          if ent.name == resource.name
-             temp=ent.name
-             break
-          end
+    # Password management routine for trusted and non-trusted systems
+    #temp=""
+    while ent = Etc.getpwent() do
+      if ent.name == resource.name
+        temp=ent.name
+        break
       end
-      Etc.endpwent()
-      if !temp
-          return nil
-      end
+    end
+    Etc.endpwent()
+    if !temp
+      return nil
+    end
 
-      ent = Etc.getpwnam(resource.name)
-      if ent.passwd == "*"
-          # Either no password or trusted password, check trusted
-          file_name="/tcb/files/auth/#{resource.name.chars.first}/#{resource.name}"
-          if File.file?(file_name)
-              # Found the tcb user for the specific user, now get passwd
-              File.open(file_name).each do |line|
-                  if ( line =~ /u_pwd/ )
-                      temp_passwd=line.split(":")[1].split("=")[1]
-                      ent.passwd = temp_passwd
-                      return ent.passwd
-                  end
-              end
-          else
-              debug "No trusted computing user file #{file_name} found."
+    ent = Etc.getpwnam(resource.name)
+    if ent.passwd == "*"
+      # Either no password or trusted password, check trusted
+      file_name="/tcb/files/auth/#{resource.name.chars.first}/#{resource.name}"
+      if File.file?(file_name)
+        # Found the tcb user for the specific user, now get passwd
+        File.open(file_name).each do |line|
+          if ( line =~ /u_pwd/ )
+            temp_passwd=line.split(":")[1].split("=")[1]
+            ent.passwd = temp_passwd
+            return ent.passwd
           end
+        end
       else
-          return ent.passwd
+        debug "No trusted computing user file #{file_name} found."
+      end
+    else
+      return ent.passwd
+    end
+  end
+
+  def trusted
+      trusted_sys = %x(/usr/lbin/getprpw root 2>&1)
+      if trusted_sys.chomp == "System is not trusted."
+         "NotTrusted"
+      else
+         "Trusted"
       end
   end
+
+  def trust2
+      trusted_sys = %x(/usr/lbin/getprpw root 2>&1)
+      if trusted_sys.chomp == "System is not trusted."
+         false
+      else
+         true
+      end
+  end
+
+
+  def password_min_age
+    ent= Etc.getpwnam(resource.name)
+    temp = %x( /usr/lbin/getprdef -m mintm ).chomp
+    mintm = temp[/mintm=(.*)/, 1]
+    if mintm == ""
+       return nil
+    else
+       return mintm
+    end
+
+  end
+
+  def password_max_age
+    temp = %x( /usr/lbin/getprdef -m exptm ).chomp
+    maxtm = temp[/exptm=(.*)/,1 ]
+    if maxtm == ""
+       return nil
+    else
+       return maxtm
+    end
+  end
+
 end
--- spec/unit/provider/user/hpux_spec.rb
+++ spec/unit/provider/user/hpux_spec.rb
@@ -1,45 +1,105 @@
 #!/usr/bin/env ruby
 require 'spec_helper'
+require 'etc'
+require 'pp'
 
 provider_class = Puppet::Type.type(:user).provider(:hpuxuseradd)
 
 describe provider_class do
-  # left from the useradd test... I have no clue what I'm doing.
-  before do
-    @resource = stub("resource", :name => "myuser", :managehome? => nil, :should => "fakeval", :[] => "fakeval")
-    @provider = provider_class.new(@resource)
+  let :resource do
+    Puppet::Type.type(:user).new(
+      :title => 'testuser',
+      :comment => 'Test J. User',
+      :provider => :hpuxuseradd
+    )
   end
-
+  let(:provider) { resource.provider }
   it "should add -F when modifying a user" do
-    @resource.expects(:allowdupe?).returns true
-    @provider.expects(:execute).with { |args| args.include?("-F") }
-    @provider.uid = 1000
+    resource.stubs(:allowdupe?).returns true
+    provider.expects(:execute).with { |args| args.include?("-F") }
+    provider.uid = 1000
   end
 
   it "should add -F when deleting a user" do
-    @provider.stubs(:exists?).returns(true)
-    @provider.expects(:execute).with { |args| args.include?("-F") }
-    @provider.delete
+    provider.stubs(:exists?).returns(true)
+    provider.expects(:execute).with { |args| args.include?("-F") }
+    provider.delete
   end
 
-end
+  context "managing passwords" do
+    let :pwent do
+      Struct::Passwd.new("testuser", "foopassword")
+    end
 
-describe provider_class do
+    before :each do
+      Etc.stubs(:getpwent).returns(pwent)
+      Etc.stubs(:getpwnam).returns(pwent)
+    end
 
-  it "should have feature manages_passwords" do
+    it "should have feature manages_passwords" do
       provider_class.should be_manages_passwords
+    end
+
+    it "should return nil if user does not exist" do
+      Etc.stubs(:getpwent).returns(nil)
+      provider.password.must be_nil
+    end
+
+    it "should return password entry if exists" do
+      provider.password.must == "foopassword"
+    end
   end
 
-  it "should return nil if user does not exist" do
-      @resource = stub("resource", :name => "no_user")
-      @provider = provider_class.new(@resource)
-      @provider.password.must be_nil
+  context "managing passwords 2" do
+    let :pwent do
+      Struct::Passwd.new("root", "bazpassword")
+    end
+    before :each do
+      Etc.stubs(:getpwent).returns(pwent)
+      Etc.stubs(:getpwnam).returns(pwent)
+    end
+
+     it "Should have feature manages_password_age" do
+        provider_class.should be_manages_password_age
+     end
+
+     it "Should have feature manages_expiry" do
+        provider_class.should be_manages_expiry
+     end
+
+
+     it "should return modprpw for password aging on trusted systems" do
+        if provider.trusted == "Trusted"
+           provider_class.command(:password).should == "/usr/lbin/modprpw"
+        end
+     end
+
+    it "Should be able to test for trusted computing" do
+      #provider.trusted.should be ("Trusted").or("NotTrusted")
+      provider.trusted.should_not be_nil
+    end
+
+    it "Should return min age for user if trusted system" do
+       if provider.trusted == "Trusted"
+          provider.password_min_age.should_not be_nil
+       end
+    end
+
+    it "Should return max age for user if trusted system" do
+       if provider.trusted == "Trusted"
+          provider.password_max_age.should_not be_nil
+       end
+    end
+
+  it "should add /usr/lbin/modprpw -v -l when modifying user if trusted" do
+    if provider.trust2
+       resource.stubs(:allowdupe?).returns true
+       provider.expects(:execute).with() { |args|  args.include?('/usr/lbin/modprpw') and args.include?("-v") and args.include?("-l") }
+       provider.uid = 1000
+    end
   end
 
 
-  it "should return password entry if exists" do
-      @resource = stub("resource", :name => "root")
-      @provider = provider_class.new(@resource)
-      @provider.password.should be_true
   end
+
 end
